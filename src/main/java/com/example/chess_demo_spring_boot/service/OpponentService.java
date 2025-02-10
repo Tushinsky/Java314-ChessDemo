4d6ad81da2df8f0d52ee394955ca78183c0afa55
Sergey Tushinsky <sergeytushinsky@i.ua>
2025-02-10 20:50:24 +0300

    удалены неиспользуемые импорты классов

--- src/main/java/com/example/chess_demo_spring_boot/service/OpponentService.java
+++ src/main/java/com/example/chess_demo_spring_boot/service/OpponentService.java
@@ -5,7 +5,6 @@ import com.example.chess_demo_spring_boot.domain.ChessParty;
 import com.example.chess_demo_spring_boot.domain.Opponent;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OpponentService {
     List<Opponent> getAllByChessMan(ChessMan chessMan);
